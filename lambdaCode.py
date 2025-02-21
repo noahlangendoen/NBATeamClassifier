de382f1fe2958ce6b5cbeff833d29f6983e04bd9
Noah Langendoen <noah.langendoen@gmail.com>
2025-02-20 20:07:23 -0600

    Finished lambdaCode

--- lambdaCode.py
+++ lambdaCode.py
@@ -1,6 +1,7 @@
 import json
 import os
 import boto3
+from botocore.exceptions import ClientError
 
 s3 = boto3.client("s3")
 s3Bucket = "seniorprojectbucketnoahl"
@@ -19,8 +20,24 @@ def eventHandler(event, context):
                 "body": json.dumps(upload)
             }
         
+        response= {
+            'success': True,
+            's3_url': presignedURL(s3Bucket, filename)
+        }
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response)
+        }
+        
     except Exception as e:
         return {
-            "statusCode": 500,
-            "body": json.dumps({"error": str(e)})
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
         }
+    
+def presignedURL(bucketName, objectName, expiration=3600):
+    try:
+        return s3.generate_presigned_url('put_object', Params={'Bucket': bucketName, 'Key': objectName}, ExpiresIn=expiration)
+    except ClientError as e:
+        raise Exception(f'Error generating presigned URL: {e}')
