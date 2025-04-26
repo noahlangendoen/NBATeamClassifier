ce30adfea6391eab1fe3ac94396e5e01082c632c
Noah Langendoen <noah.langendoen@gmail.com>
2025-04-26 12:11:55 -0500

    Finished GUI

--- MyGUI.py
+++ MyGUI.py
@@ -1,3 +1,5 @@
+# This file is the GUI used for my demonstration of my trained model
+
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QFileDialog, QLabel
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
@@ -5,44 +7,38 @@ import sys
 import numpy as np
 from demoFuncs import predictImage
 
-
-
 class DemoGUI(QMainWindow):
+
+    # Constructor for the GUI
     def __init__(self):
         super().__init__()
         self.setWindowTitle("NBA Team Classifier")
-        self.setFixedSize(400, 400)
+        self.setFixedSize(1000, 1000)
 
-        self.upload_button = QPushButton("Upload Image")
-        self.upload_button.clicked.connect(self.uploadFile)
+        self.uploadButton = QPushButton("Upload Image")
+        self.uploadButton.clicked.connect(self.uploadFile)
 
-        self.image_label = QLabel()
-        self.image_label.setAlignment(Qt.AlignCenter)
-
-        self.prediction_label = QLabel("")
-        self.prediction_label.setAlignment(Qt.AlignCenter)
-        self.prediction_label.setStyleSheet("font-size: 24px; color: black;")
+        self.imageLabel = QLabel()
+        self.imageLabel.setAlignment(Qt.AlignCenter)
 
         layout = QVBoxLayout()
-        layout.addWidget(self.upload_button)
-        layout.addWidget(self.image_label)
-        layout.addWidget(self.prediction_label)
+        layout.addWidget(self.uploadButton)
+        layout.addWidget(self.imageLabel)
 
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
 
+    # This function is called when the upload button is clicked, and it is responsible for calling the demoFuncs.py file
     def uploadFile(self):
-        file_path, _ = QFileDialog.getOpenFileName(self, "Select Image File", "", "Images (*.png *.jpg *.jpeg)")
-        if file_path:
-            self.image_label.adjustSize()
-            team, annotatedImg = predictImage(file_path)
+        filePath, _ = QFileDialog.getOpenFileName(self, "Select Image File", "", "Images (*.png *.jpg *.jpeg)")
+        if filePath:
+            self.imageLabel.adjustSize()
+            annotatedImg = predictImage(filePath)
             pixmap = QPixmap(annotatedImg)
-            self.image_label.setPixmap(pixmap.scaled(300, 300, Qt.KeepAspectRatio))
-            self.prediction_label.setText(team)
+            self.imageLabel.setPixmap(pixmap.scaled(800, 800, Qt.KeepAspectRatio))
 
-
-
+# Load the GUI when the file is run
 app = QApplication(sys.argv)
 window = DemoGUI()
 window.show()
--- demoFuncs.py
+++ demoFuncs.py
@@ -1,10 +1,13 @@
-from PIL import Image, ImageDraw
+# This file is used to load models, make predictions, and return the annotated image for display the MyGUI.py file.
+
+from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import torch
 from models.evenbetternet import EvenBetterNet
 import torchvision.transforms as transforms
 from ultralytics import YOLO
 
+# This function loads my pre-trained model and the YOLOv8 model
 def loadModel():
     model1 = EvenBetterNet()
     model1.load_state_dict(torch.load('best_modelReLU.pt', map_location=torch.device('cpu')))
@@ -13,36 +16,58 @@ def loadModel():
 
     return model1, model2
 
-def preprocessImage(imgPath):
+# This function preprocesses the image to be compatible with the model input, and normallizes the image for prediction
+def preprocessImage(image):
+    # Resize the image to 224x224 and normalize it
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
-    image = Image.open(imgPath).convert('RGB')
     image = transform(image).unsqueeze(0)
     return image
 
+# This function takes an image path as input, loads the models, makes predictions, and draws bounding boxes around the detected objects
 def predictImage(imgPath):
-    teams = ["76ers", "Bucks", "Bulls", "Cavaliers", "Celtics", "Clippers", "Grizzlies", "Hawks", "Heat", "Hornets", "Jazz", "Kings", "Knicks", "Lakers", "Magic", "Mavericks", "Nets", "Nuggets", "Pacers", "Pelicans", "Pistons", "Raptors", "Rockets", "Spurs","Suns", "Thunder", "Timberwolves", "Trail Blazers", "Warriors", "Wizards"]
+    # List of NBA teams to map the predicted labels
+    teams = ["76ers", "Bucks", "Bulls", "Cavaliers", "Celtics", "Clippers", "Grizzlies", "Hawks", "Heat", "Hornets", "Jazz", "Kings", "Knicks", "Lakers", "Magic", "Mavericks", "Nets", "Nuggets", "Pacers", "Pelicans", "Pistons", "Raptors", "Rockets", "Spurs", "Suns", "Thunder", "Timberwolves", "Trail Blazers", "Warriors", "Wizards"]
+    # Load the models, make the predictions, and draw the bounding boxes
     model1, model2 = loadModel()
-    image = preprocessImage(imgPath)
-    with torch.no_grad():
-        outputs = model1(image)
-
-    _, predicted = torch.max(outputs, 1)
-    team = teams[predicted.item()]
-    
     results = model2(imgPath)[0]
     img = Image.open(imgPath).convert('RGB')
     boxes = results.boxes.xyxy.cpu().numpy()
     draw = ImageDraw.Draw(img)
-    for box in boxes:
-        x1, y1, x2, y2 = map(int, box[:4])
-        draw.rectangle([x1, y1, x2, y2], outline="red", width=3)
-    
-    annotatedImg = "annotated_image.jpg"
-    img.save(annotatedImg)  # Save the annotated image to disk
+    font = ImageFont.truetype("arial.ttf", size=64)
 
-    return team, annotatedImg
+    # Creating a list to store predictions
+    predictions = []
+
+    # Looping through the detected boxes, making the prediction, drawing the boxes and the labels on the image
+    for box in boxes:
+        # Extracting the coordinates of the bounding box
+        x1, y1, x2, y2 = map(int, box[:4])
+        croppedImg = img.crop((x1, y1, x2, y2))
+        croppedTensor = preprocessImage(croppedImg)
+
+        # Making the prediction using my trained model
+        with torch.no_grad():
+            outputs = model1(croppedTensor)
+        _, predicted = torch.max(outputs, 1)
+        team = teams[predicted.item()]
+
+        # Appending the prediction and the boxes coordinates to the predictions list
+        predictions.append((team, (x1, y1, x2, y2)))
+        draw.rectangle([x1, y1, x2, y2], outline="red", width=6)
+
+        # Drawing the label and the box on the image
+        bbox = draw.textbbox((0, 0), team, font=font)
+        width = bbox[2] - bbox[0]
+        centerX = (x1 + x2) // 2
+        textX = centerX - (width // 2)
+        draw.text((textX, y1), team, fill="red", font=font)
+
+    annotatedImg = "annotated_image.jpg"
+    img.save(annotatedImg)
+
+    return annotatedImg
